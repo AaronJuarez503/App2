0f2bb9af39a7dc6c7abd10c0b30c5f15c7147ac9
AaronJuarez503 <aaronraymundo00@gmail.com>
2024-08-01 18:36:42 -0500

    A

---
+++ controller/index.js
@@ -0,0 +1,8 @@
+var conexion = require('../config/index')
+var consulta = require('../model/index')
+
+module.exports={
+
+   
+   
+}//fin de modules exports no idont delet
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var controlador =require('../controller/index')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('registro');
 });
 
-module.exports = router;
+module.exports = router;//no borrar 
