e6b662df35bc49c5109ad075de23fc27ae423b80
AaronJuarez503 <aaronraymundo00@gmail.com>
2024-08-02 23:49:17 -0500

    A

--- model/index.js
+++ model/index.js
@@ -1,17 +1,16 @@
 module.exports={
 
-    RegistrarCliente:function(conexion,{nom,apell,user,correo,pass}) {
-        const insertar = `INSERT INTO cliente (nombre, apellido, usuario, correo, contraseña) VALUES ('${nom}, ${apell}, ${user}, ${correo}, ${pass}')`;
-        return new Promise((Resolve, reject) => {
-            conexion.query(insertar, function (error, results) {
+    RegistrarCliente:function(conexion, {nom, apell, user, correo, pass}) {
+        const insertar = `INSERT INTO cliente (nombre, apellido, usuario, correo, contraseña) VALUES ('${nom}', '${apell}', '${user}', '${correo}', '${pass}')`;
+        return new Promise((resolve, reject) => {
+            conexion.query(insertar, function (error, resultado) {
                 if (error) {
                     reject(error);
                 } else {
-                    Resolve(results);
+                    resolve(resultado);
                 }
             });
-        })
-        
+        });
     }
 
 }
