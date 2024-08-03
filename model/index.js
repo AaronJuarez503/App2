20a75b3e8bcd46ad2c92eb78c7cb0c95804a4d39
Alan <6691487@clases.edu.sv>
2024-08-02 19:22:39 -0600

    hh

--- model/index.js
+++ model/index.js
@@ -1,7 +1,7 @@
 module.exports={
 
     RegistrarCliente:function(conexion,{nom,apell,user,correo,pass}) {
-        const insertar = `INSERT INTO cliente (nombre, apellido, usuario, correo, contraseña) VALUES ('${nom}, ${apell}, ${user}, ${correo}, ${pass}')`;
+        const insertar = `INSERT INTO cliente (nombre, apellido, usuario, correo, contraseña) VALUES ('${nom}', '${apell}', '${user}', '${correo}', '${pass}')`;
         return new Promise((Resolve, reject) => {
             conexion.query(insertar, function (error, results) {
                 if (error) {
