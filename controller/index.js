cedb189a7100344f3c0ad993ec848fbf3ac8967a
Alan <6691487@clases.edu.sv>
2024-08-06 21:51:53 -0600

    ttt

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,8 @@
 var conexion = require('../config/index')
 var consulta = require('../model/index')
-var correo= require('../controller/enviargmail')
+var email= require('../controller/enviargmail')
 
-
+var gcodigo;
 
 module.exports={
 
@@ -41,13 +41,63 @@ module.exports={
         consulta.RecuperarCuenta(conexion, {correo})
         .then(datos => {
             console.log('cuenta encontrada', datos)
-             res.redirect('/verificar_codigo')
+
+            
+             async function enviar () {
+            
+            
+                try {
+                  
+                    const codigo = await email.generarcodigo()
+                    console.log('tu codigo es : ' + codigo)
+                    gcodigo = codigo;
+                   // req.session.codigo[req.body.email] = codigo
+                   
+                   // console.log(req.session.codigo[req.body.email])
+                   // console.log( "::::" + Object.keys(Verificacioncodes)+":::"+Object.values(Verificacioncodes) )
+                  // res.render('login/codigo',{correo: req.session.correo})
+                  res.redirect('/verificar_codigo')
+                    const respuesta = await email.enviaremail(req.body.correo,codigo)
+                   
+                    console.log('Correo enviado correctamente : '+ respuesta);
+                } catch (error) {
+                    console.error('Error al enviar el correo : ', error);
+                    res.status(500).send('Error al enviar el correo');
+                    
+                }
+            }
+    
+         
+              enviar();
+
+
+
         })
         .catch(error => {
             console.error('error al encontrar la cuenta', error)
             res.status(500).send('error al encontrar la cuenta')
         })
 
+
+        
+
+
+
+    },
+    vcodigo:function(req,res){
+        console.log(req.body)
+        console.log("codigo generado :"+gcodigo)
+
+        if (req.body.codigo==gcodigo) {
+             res.redirect('/nueva_pass');
+        } else {
+            
+             res.send("El codigo es incorrecto");
+            
+        }
+
+        
+        
     },
     Nuevacontra:function(req, res){
         const {pass} = req.body;
--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,12 @@ router.post('/registrar',validator.Pregistrarse,controlador.RegistrarCliente)
 router.get('/recuperar_cuenta', function(req, res){
   res.render('recuperacion')
 });
-router.post('/recuperar_cuenta',validator.Precuperar,controlador.RecuperarCuenta)
+router.post('/recuperar_cuenta',validator.Precuperar,controlador.RecuperarCuenta)//validar correo
 
-router.get('/verificar_codigo', function(req, res){
+router.get('/verificar_codigo', function(req, res){ //introdusircodgio
   res.render('codigo')
 });
+router.post('/vcodigo',controlador.vcodigo)//validar codigo
 
 router.get('/nueva_pass', function(req, res){
   res.render('updata_contra')
