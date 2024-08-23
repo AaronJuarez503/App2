53cfb5a5752a47444c47763782222e64effe9a5a
Alan <6691487@clases.edu.sv>
2024-08-23 17:47:21 -0600

    hh

---
+++ public/javascript/product.js
@@ -0,0 +1,46 @@
+$(function(){
+
+    var id=$('buton');
+
+    $.ajax({
+        url: `/users/pr0ducts${id}`,
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            
+            console.log('Datos recibidos:', data);
+          function crearDivConImagen(clase, texto, urlImagen) {
+            var $div = $('<div>', { class: clase });
+            var $img = $('<img>', { src:urlImagen , alt: texto });
+            var $p = $('<p>', { text: texto });
+            $div.append($img).append($p);
+            return $div;
+            
+        }
+        data.forEach(function(item) {
+            var $itemDiv = crearDivConImagen('item-con-imagen', item.descripcion, item.imagen);
+            $('#contenedor').append($itemDiv);})
+
+
+            
+
+
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+
+                for (let index = 0; index < array.length; index++) {
+                    const element = array[index];
+                    
+                }
+                
+            }
+            
+        },
+
+        error: function(xhr, status, error) {
+            console.error('Error en la petición AJAX:', error);
+            $('#resultado').html('Error al cargar los datos');
+        }
+    });
+
+})
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,9 @@ var controller=require('../controller/index2')
 /* GET users listing. */
 router.get('/',controller.marcas);
 
+
+router.get('/pr0ducts/:id',controller.marcas);
+
+
+
 module.exports = router;
