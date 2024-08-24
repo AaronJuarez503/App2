8d366c4d7df228b135eef8d2f7f054173cbfa2a3
AaronJuarez503 <aaronraymundo00@gmail.com>
2024-08-23 23:48:37 -0500

    A

--- public/javascript/product.js
+++ public/javascript/product.js
@@ -1,30 +1,64 @@
 $(function(){
 
-    var id=$('buton');
+    
 
     $.ajax({
-        url: `/users/pr0ducts`,
+        url: `/users/products`,
         method: 'GET',
         dataType: 'json',
         success: function(data) {
             
             console.log('Datos recibidos:', data);
-         /* function crearDivConImagen(clase, texto, urlImagen) {
-            var $div = $('<div>', { class: clase });
-            var $img = $('<img>', { src:urlImagen , alt: texto });
-            var $p = $('<p>', { text: texto });
-            $div.append($img).append($p);
-            return $div;
+
+            function crearDivConElementos(clase, textoH3, textoSpan, textoP, textoButton1, textoButton2, textoButton3) {
+                var $div = $('<div>', { class: clase });
+                var $h3 = $('<h3>', { text: textoH3 });
+                var $span = $('<span>', { text: textoSpan });
+                var $p = $('<p>', { text: textoP });
+                var $button1 = $('<button>', { text: textoButton1 });
+                var $input = $('<input>', { type: 'text', value: '0', readonly: true });
+                var $button2 = $('<button>', { text: textoButton2 });
+                var $button3 = $('<button>', { text: textoButton3 });
+
+                $button1.on('click', function() {
+                    var currentValue = parseInt($input.val(), 10);
+                    if (currentValue > 0) {
+                        $input.val(currentValue - 1);
+                    }
+                });
+
+                $button2.on('click', function() {
+                    var currentValue = parseInt($input.val(), 10);
+                    $input.val(currentValue + 1);
+                });
+
+                $button3.on('click', function() {
+                    var cantidad = parseInt($input.val(), 10);
+                    if (cantidad > 0) {
+                        alert(`${textoH3} agregado al carrito con cantidad: ${cantidad}`);
+                    } else {
+                        alert('Por favor, selecciona una cantidad mayor a 0.');
+                    }
+                });
+
+                $div.append($h3)
+                .append($span)
+                .append($p)
+                .append($button1)
+                .append($input)
+                .append($button2)
+                .append($button3);
+                return $div;
             
         }
         data.forEach(function(item) {
-            var $itemDiv = crearDivConImagen('item-con-imagen', item.descripcion, item.imagen);
-            $('#contenedor').append($itemDiv);})
+            var $itemDiv = crearDivConElementos('item-con-imagen', item.nombre, item.precio, item.descripcion,'decrement', 'increment', 'agregar al carrito');
+            $('#mio').append($itemDiv);})
 
 
             
 
-
+             /*
             for (let index = 0; index < array.length; index++) {
                 const element = array[index];
 
