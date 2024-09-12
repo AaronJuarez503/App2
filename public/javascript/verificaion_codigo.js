7f2a02cbab91527d0dea7d818dded10e0934e684
Alan <6691487@clases.edu.sv>
2024-09-12 08:37:35 -0600

    ww

--- public/javascript/verificaion_codigo.js
+++ public/javascript/verificaion_codigo.js
@@ -12,6 +12,13 @@ $(function () {
             method: 'GET',
             success: function(response) {
                 console.log('Respuesta exitosa:', response);
+                if (response) {
+                    $('.holi').text('Al pareser ya has realizados compra anteriormente te pedidos que nos brindes tu codigo de verificacion')
+                    
+                } else {
+                    $('.holi').text('Al pareser es la rpimera ves que realizas una compra por favor ingrege el codigo que la empresa te proporiciono si no lo tienes puedes perdilo ')
+                }
+
                 
             },
             error: function(xhr, status, error) {
